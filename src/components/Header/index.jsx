4673fa6ead2d2c85cee709b78ae31696fde7e857
Douglas Xavier Viégas <douglasviegas2011@hotmail.com>
2025-03-03 19:07:34 -0300

    Configurações de links header social

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,40 +7,40 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export const Header = () => {
     return (
       <header>
-        <div className={styles.logo}>
-          <img src={Logo_White} alt="Logo Trip For Me Branco" />
-        </div>
-        <nav className={styles.nav_links}>
-          <ul className={styles.nav_list}>
-            <li><a href="">Início</a></li>
-            <li><a href="">Viagens Internacionais</a></li>
-            <li><a href="">Viagens Nacionais</a></li>
-            <li><a href="">Por que a Trip For Me</a></li>
-          </ul>
-        </nav>
-        <ul className={styles.social_list}>
-          <li>
-            <a href="">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <FontAwesomeIcon icon={faWhatsapp} />
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </a>
-          </li>
-        </ul>
-        <button className={styles.bt_personalizar}>
-          <a> Viagem do Seu Jeito </a>
-          <div>
-            <FontAwesomeIcon icon={faArrowRight} />
-          </div>
-        </button>
+      <div className={styles.logo}>
+      <img src={Logo_White} alt="Logo Trip For Me Branco" />
+      </div>
+      <nav className={styles.nav_links}>
+      <ul className={styles.nav_list}>
+      <li><a href="/">Início</a></li>
+      <li><a href="">Viagens Internacionais</a></li>
+      <li><a href="">Viagens Nacionais</a></li>
+      <li><a href="">Por que a Trip For Me</a></li>
+      </ul>
+      </nav>
+      <ul className={styles.social_list}>
+      <li>
+      <a href="https://www.instagram.com/tripformee/" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
+      </li>
+      <li>
+      <a href="https://api.whatsapp.com/send?phone=5543988732858&text=Ol%C3%A1,%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20para%20minha%20viagem%20com%20voc%C3%AAs!%20Vou%20passar%20as%20informa%C3%A7%C3%B5es%20aqui%20abaixo:" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faWhatsapp} />
+      </a>
+      </li>
+      <li>
+      <a href="mailto:contato@tripforme.com.br" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+      </li>
+      </ul>
+      <button className={styles.bt_personalizar}>
+      <a> Viagem do Seu Jeito </a>
+      <div>
+      <FontAwesomeIcon icon={faArrowRight} />
+      </div>
+      </button>
       </header>
     );
 };
